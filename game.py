da794c1715f1a1ed86744ef6208daae6523a9466
sedthh <sedthh@gmail.com>
2018-10-12 17:02:42 +0200

    subjects and experimenters can join the same session with different devices
    - game now allows joining in with different devices (multiple connections) to the same places
    - settings.ini now has more game environemnt info
    - game logic is handled, added AI
    - udpated tmp\echo for testing

--- game.py
+++ game.py
@@ -4,11 +4,30 @@ import os
 import configparser
 from numpy import random
 from sys import exit
+from copy import deepcopy
 
 STRATEGIES = ["COOPERATE", "DEFECT", "RANDOM", "TIT-FOR-TAT", "ALTERNATING"]
 STRATEGY = "RANDOM"
+SUBJECT_CHOICES = []
+EXPERIMENTER_CHOICES = []
+AIS = []
+ENVIRONMENTS = []
+MAX_GAMES_VR = 0
+MAX_GAMES_BEFORE = 0
+MAX_GAMES_AFTER = 0
+
+def shuffle(my_list,expand=True):
+	if my_list:
+		output_list = []
+		while len(output_list) < MAX_GAMES_VR:
+			random.shuffle(my_list)
+			output_list += my_list
+			if not expand:
+				break
+		return output_list[:MAX_GAMES_VR]
 
 def get_settings(file):
+	global MAX_GAMES_VR
 	config = os.path.join(os.path.dirname(os.path.realpath(__file__)), file)
 	if os.path.isfile(config):
 		settings = configparser.ConfigParser()
@@ -18,36 +37,147 @@ def get_settings(file):
 				ip = settings["SERVER"]["IP"]
 				port = int(settings["SERVER"]["PORT"])
 				log_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), settings["SERVER"]["LOG_FILE"])
-				return ip, port, log_file
+				wait = settings["SERVER"]["WAIT"]
+				games = int(settings["SERVER"]["MAX_GAMES_BEFORE"]) + int(settings["SERVER"]["MAX_GAMES_VR"]) + int(settings["SERVER"]["MAX_GAMES_AFTER"])
+				get_data(file)
+				return ip, port, log_file, wait, games
 			except KeyError as error:
 				exit(f"Configuration file missing argument: {str(error)}")
 	exit(f"Missing or malformed configuration file: {file}")
 
-def get_name(file,sex=0):
-	return get_data(file,"NAMES",sex)
-
-def get_avatar(file,sex=0):
-	return get_data(file, "AVATARS", sex)
-
-def get_data(file,section,value):
+def get_data(file):
+	global AIS, ENVIRONMENTS, MAX_GAMES_VR, MAX_GAMES_BEFORE, MAX_GAMES_AFTER
 	config = os.path.join(os.path.dirname(os.path.realpath(__file__)), file)
 	if os.path.isfile(config):
 		settings = configparser.ConfigParser()
 		settings.read(config)
-		if section in settings:
-			choices = [name for name in settings[section] if int(settings[section][name]) == value]
-			if choices:
-				return random.choice(choices)
-	raise Exception("Could not find data with value={value} in {section}")
+		MAX_GAMES_VR = int(settings["SERVER"]["MAX_GAMES_VR"])
+		MAX_GAMES_BEFORE = int(settings["SERVER"]["MAX_GAMES_BEFORE"])
+		MAX_GAMES_AFTER = int(settings["SERVER"]["MAX_GAMES_AFTER"])
+
+		males = {
+			"names": shuffle([key for key in settings["NAMES"] if int(settings["NAMES"][key]) == 1], False),
+			"avatars": shuffle([key for key in settings["AVATARS"] if int(settings["AVATARS"][key]) == 1], False)
+		}
+		females = {
+			"names": shuffle([key for key in settings["NAMES"] if int(settings["NAMES"][key]) == 0], False),
+			"avatars": shuffle([key for key in settings["AVATARS"] if int(settings["AVATARS"][key]) == 0], False)
+		}
+
+		AIS += zip(males["names"],males["avatars"])
+		AIS += zip(females["names"], females["avatars"])
+		AIS = [("", "") for i in range(MAX_GAMES_BEFORE)] + shuffle(AIS) + [("", "") for i in range(MAX_GAMES_AFTER)]
+		ENVIRONMENTS = shuffle([settings["ENVIRONMENTS"][key] for key in settings["ENVIRONMENTS"]])
+		ENVIRONMENTS = ["" for i in range(MAX_GAMES_BEFORE)] + ENVIRONMENTS + ["" for i in range(MAX_GAMES_AFTER)]
+
+def get_name(game):
+	return AIS[game][0]
+
+def get_avatar(game):
+	return AIS[game][1]
+
+def get_environment(game):
+	return ENVIRONMENTS[game]
 
 def get_strategy():
 	return random.choice(STRATEGIES)
 
-def get_game(file,sex):
+def get_game(game=0):
 	global STRATEGY
 	response = {
-		"name": get_name(file, sex),
-		"avatar": get_avatar(file, sex)
+		"name": get_name(game),
+		"avatar": get_avatar(game),
+		"environment": get_environment(game),
+		"rotation": random.randint(0, 360)
 	}
 	STRATEGY = get_strategy()
 	return response
+
+def get_test(subject, experimenter, game=0):
+	game_data = get_game(game)
+	subject["data"]["environment"] = game_data["environment"]
+	experimenter["data"]["environment"] = game_data["environment"]
+	subject["data"]["rotation"] = game_data["rotation"]
+	experimenter["data"]["rotation"] = (game_data["rotation"]+180)%360
+	experimenter["data"]["name"] = game_data["name"]
+	experimenter["data"]["avatar"] = game_data["avatar"]
+	loading = int(random.choice([1, 2, 3]))
+	subject["data"]["loading"] = loading
+	experimenter["data"]["loading"] = loading
+	subject["data"]["message"] = "Searching for match."
+	experimenter["data"]["message"] = "Searching for match."
+	rounds_to_play = random.randint(3, 8)
+	subject["data"]["game"]["rounds_left"] = rounds_to_play
+	experimenter["data"]["game"]["rounds_left"] = rounds_to_play
+	subject["data"]["choice"] = ""
+	experimenter["data"]["choice"] = ""
+	return subject, experimenter
+
+def play_once(subject, experimenter):
+	global SUBJECT_CHOICES, EXPERIMENTER_CHOICES
+	SUBJECT_CHOICES.append(subject["data"]["choice"])
+	EXPERIMENTER_CHOICES.append(experimenter["data"]["choice"])
+
+	subject_score, experimenter_score = get_game_results(subject["data"]["choice"], experimenter["data"]["choice"])
+	game = {
+		"rounds_left": max(subject["data"]["game"]["rounds_left"],experimenter["data"]["game"]["rounds_left"])-1,
+		"SUBJECT": {
+			"choice": subject["data"]["choice"],
+			"score": subject["data"]["game"]["SUBJECT"]["score"]+subject_score,
+			"score_all": subject["data"]["game"]["SUBJECT"]["score_all"]+subject_score
+		},
+		"EXPERIMENTER": {
+			"choice": experimenter["data"]["choice"],
+			"score": experimenter["data"]["game"]["EXPERIMENTER"]["score"] + experimenter_score,
+			"score_all": experimenter["data"]["game"]["EXPERIMENTER"]["score_all"] + experimenter_score
+		}
+	}
+	subject["data"]["choice"] = ""
+	experimenter["data"]["choice"] = ""
+	subject["data"]["game"] = deepcopy(game)
+	experimenter["data"]["game"] = deepcopy(game)
+	return subject, experimenter
+
+def get_game_results(a, b):
+	# payoff matrix
+	if a == "cooperate":
+		if b == "cooperate":
+			a_score = 3
+			b_score = 3
+		else:
+			a_score = 0
+			b_score = 5
+	else:
+		if b == "cooperate":
+			a_score = 5
+			b_score = 0
+		else:
+			a_score = 1
+			b_score = 1
+	return a_score, b_score
+
+def get_choice():
+	if STRATEGY == "COOPERATE":
+		return "cooperate"
+	elif STRATEGY == "DEFECT":
+		return "defect"
+	elif STRATEGY == "TIT-FOR-TAT":
+		if SUBJECT_CHOICES:
+			return SUBJECT_CHOICES[-1]
+		else:
+			return "cooperate"
+	elif STRATEGY == "ALTERNATING":
+		if EXPERIMENTER_CHOICES:
+			if EXPERIMENTER_CHOICES[-1] == "cooperate":
+				return "defect"
+			else:
+				return "cooperate"
+		else:
+			return "cooperate"
+	else:
+		return random.choice(["cooperate", "defect"])
+
+if __name__ == "__main__":
+	_,_,_,_,_ =get_settings("settings.ini")
+	for i in range(MAX_GAMES_VR+2):
+		print(get_game(i))
--- server.py
+++ server.py
@@ -5,7 +5,6 @@ import websockets
 import time
 import codecs
 import json
-from numpy import random
 
 import game
 SETTINGS = "settings.ini"
@@ -21,27 +20,62 @@ def log(message):
 ##########
 
 async def vr_server(websocket, path):
-	global CONNECTIONS, READY
+	global CONNECTIONS, READY, GAMES_PLAYED
 	client_type = "UNKNOWN"
 	client_ip = "0.0.0.0"
+	is_waiting = False
 	if websocket.remote_address:
 		client_ip = str(websocket.remote_address[0])
 	log(f"Incoming connection: {client_ip}")
 	try:
 		while True:
 			data = json.loads(await websocket.recv())
-			response = {
-				"timestamp": time.time()
-			}
 			# first connection
 			if client_type == "UNKNOWN" and "type" in data and data["type"] in ("SUBJECT", "EXPERIMENTER"):
 				client_type = data["type"]
-				CONNECTIONS[client_type] = {
-					"socket": websocket,		# send data from other connections here
-					"session": time.time(),		# time of connection established
-					"choice": "",
-					"ready": False				# ready to take part in the experiment
-				}
+				if client_type in CONNECTIONS:
+					CONNECTIONS[client_type]["socket"].append(websocket)
+				else:
+					CONNECTIONS[client_type] = {
+						"socket": [websocket],		# send data from other connections here
+						"session": time.time(),		# time of connection established
+						"ready": False,  			# ready to take part in the experiment
+						"data": {
+							"timestamp": 0,				# current timestamp (gets updated)
+							"name": "Unknown",
+							"avatar": "default",
+							"environment": "default",	# current environment
+							"rotation": 0,				# rotation of environment
+							"loading": 0,				# show fake loading for this long
+							"message": "Connected.",	# messages for debugging
+							"choice": "",				# the last sent choice (["game"]["me"]["choice"] will also have its value)
+							"game": {
+								"rounds_left": 0,
+								"SUBJECT": {
+									"choice": "",
+									"score": 0,
+									"score_all": 0
+								},
+								"EXPERIMENTER": {
+									"choice": "",
+									"score": 0,
+									"score_all": 0
+								}
+							},
+							"transform": {				# position and rotation of headset
+								"pos": {
+									"x": 0.0,
+									"y": 0.0,
+									"z": 0.0
+								},
+								"rot": {
+									"x": 0.0,
+									"y": 0.0,
+									"z": 0.0
+								}
+							}
+						}
+					}
 				log(f"{client_ip} is identified as {client_type}")
 
 			# connection is ready
@@ -52,42 +86,127 @@ async def vr_server(websocket, path):
 						if not READY:
 							READY = True
 							log(f"Both subject and experimenter have joined the session")
-							sex = random.choice([0, 1])
-							response["game"] = game.get_game(SETTINGS, sex)
+							CONNECTIONS["SUBJECT"], CONNECTIONS["EXPERIMENTER"] = \
+								game.get_test(CONNECTIONS["SUBJECT"], CONNECTIONS["EXPERIMENTER"], GAMES_PLAYED)
+							await send_all()
 					elif READY:
 						READY = False
 						log(f"Session interrupted, {client_type} is not ready")
 
-			# sending headset transform information
-			if "transform" in data:
-				if "pos" in data["transform"] and "rot" in data["transform"]:
-					response["transform"] = data["transform"]
-			# send message
-			if "message" in data:
-				response["message"] = data["message"]
+			# subject sets name (just save it for later)
+			if "name" in data and client_type in CONNECTIONS:
+				CONNECTIONS[client_type]["data"]["name"] = data["name"]
 
-			# send update to other client(s)
-			for connection in CONNECTIONS:
-				if connection != client_type or True:
-					if CONNECTIONS[connection]["ready"]:
-						await CONNECTIONS[connection]["socket"].send(json.dumps(response))
+			# subject sets avatar (just save it for later)
+			if "avatar" in data and client_type in CONNECTIONS:
+				CONNECTIONS[client_type]["data"]["avatar"] = data["avatar"]
+
+			# sending headset transform information when received
+			if "transform" in data and client_type in CONNECTIONS:
+				if "pos" in data["transform"] and "rot" in data["transform"]:
+					# update the headset rotation and send it to others
+					await send(client_type, {"transform": data["transform"]})
+
+			# subject sends their choice
+			if "choice" in data and data["choice"] in ("cooperate", "defect") and client_type in CONNECTIONS:
+				if not is_waiting:
+					if READY:
+						# play the game
+						if client_type == "SUBJECT":
+							# subject plays the game
+							CONNECTIONS[client_type]["data"]["choice"] = data["choice"]
+						elif client_type == "EXPERIMENTER":
+							# discard choice and get actual choice from game AI based on selected strategy
+							CONNECTIONS[client_type]["data"]["choice"] = game.get_choice()
+						# calculate results if both players have chosen
+						if "SUBJECT" in CONNECTIONS and "EXPERIMENTER" in CONNECTIONS:
+							if CONNECTIONS["SUBJECT"]["data"]["game"]["SUBJECT"]["rounds_left"] > 0:
+								if CONNECTIONS["SUBJECT"]["data"]["choice"] and CONNECTIONS["EXPERIMENTER"]["data"]["choice"]:
+									log(f'''Game played: {CONNECTIONS["SUBJECT"]["data"]["choice"]} -
+										{CONNECTIONS["EXPERIMENTER"]["data"]["choice"]}''')
+									CONNECTIONS["SUBJECT"], CONNECTIONS["EXPERIMENTER"] = \
+										game.play_once(CONNECTIONS["SUBJECT"], CONNECTIONS["EXPERIMENTER"])
+									await send_all()
+									is_waiting = True
+									time.sleep(WAIT)
+									is_waiting = False
+
+									# generate new test
+									if CONNECTIONS["SUBJECT"]["data"]["game"]["SUBJECT"]["rounds_left"] <= 0:
+										GAMES_PLAYED += 1
+										CONNECTIONS["SUBJECT"], CONNECTIONS["EXPERIMENTER"] = \
+											game.get_test(CONNECTIONS["SUBJECT"], CONNECTIONS["EXPERIMENTER"], GAMES_PLAYED)
+										await send_all()
+										log(f'Generating #{GAMES_PLAYED} game')
+					else:
+						await echo(client_type, {"message": "You are not in a match yet."})
+				else:
+					await echo(client_type, {"message": "Please wait a bit until sending in your next choice."})
 
 	except websockets.ConnectionClosed:
 		log(f"{client_ip} ({client_type}) disconnected")
 	finally:
 		if client_type in CONNECTIONS:
-			del CONNECTIONS[client_type]
+			CONNECTIONS[client_type]["socket"] = \
+				[socket for socket in CONNECTIONS[client_type]["socket"] if socket != websocket]
+			if not CONNECTIONS[client_type]["socket"]:
+				del CONNECTIONS[client_type]
 		if READY:
 			if "SUBJECT" not in CONNECTIONS or "EXPERIMENTER" not in CONNECTIONS:
 				READY = False
 				log(f"Session interrupted, {client_type} disconnected")
 
+# send certain data (update sender's data with new data) and send it to everyone else but the sender
+async def send(sender,data):
+	global CONNECTIONS
+	if sender in CONNECTIONS:
+		if CONNECTIONS[sender]["ready"]:
+			# update sender's data
+			CONNECTIONS[sender]["data"]["timestamp"] = time.time()
+			for key in data:
+				if key in CONNECTIONS[sender]["data"]:
+					CONNECTIONS[sender]["data"][key] = data[key]
+			data["timestamp"] = CONNECTIONS[sender]["data"]["timestamp"]
+			message = json.dumps(data)
+			# send to others
+			for recipient in CONNECTIONS:
+				if sender != recipient:
+					if CONNECTIONS[recipient]["ready"]:
+						for socket in CONNECTIONS[recipient]["socket"]:
+							await socket.send(message)
+
+# update sender's data and send it back to it
+async def echo(sender,data):
+	global CONNECTIONS
+	if sender in CONNECTIONS:
+		CONNECTIONS[sender]["data"]["timestamp"] = time.time()
+		for key in data:
+			if key in CONNECTIONS[sender]["data"]:
+				CONNECTIONS[sender]["data"][key] = data[key]
+		data["timestamp"] = CONNECTIONS[sender]["data"]["timestamp"]
+		message = json.dumps(data)
+		for socket in CONNECTIONS[sender]["socket"]:
+			await socket.send(message)
+
+# send everything to everyone
+async def send_all():
+	for sender in CONNECTIONS:
+		if CONNECTIONS[sender]["ready"]:
+			CONNECTIONS[sender]["data"]["timestamp"] = time.time()
+			message = json.dumps(CONNECTIONS[sender]["data"])
+			for recipient in CONNECTIONS:
+				if sender != recipient:
+					if CONNECTIONS[recipient]["ready"]:
+						for socket in CONNECTIONS[recipient]["socket"]:
+							await socket.send(message)
+
 #########
 
 if __name__ == "__main__":
-	IP, PORT, LOG_FILE = game.get_settings(SETTINGS)
+	IP, PORT, LOG_FILE, WAIT, MAX_GAMES = game.get_settings(SETTINGS)
 	CONNECTIONS = {}
 	READY = False
+	GAMES_PLAYED = 0
 	SESSION = 0 # make sure this increases every time
 	log(f"Starting game session {SESSION}")
 	log(f"Server started at {IP}:{PORT}")
