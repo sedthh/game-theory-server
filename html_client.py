0998e088d31297aefe2f7dd55c18585bce82ba2e
sedthh <sedthh@gmail.com>
2019-07-12 21:43:59 +0200

    wrote new server/client prototype for new asyncio
    - rewrote server to resemble a chat room
    - server works with python 3.7 and new asyncio

---
+++ html_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import json
+
+
+async def hello():
+    async with websockets.connect(f'ws://192.168.0.241:8765') as ws:
+        msg = [
+            {"room": "system", "cmd": "login", "data": {"user": "Tibi", "pass": "1234"}},
+            {"room": "main", "cmd": "join", "data": ""},
+            {"room": "main", "cmd": "msg", "data": "hello"},
+            {"room": "main", "cmd": "transform", "data": {
+                "pos": {
+                    "x": 0.0,
+                    "y": 0.0,
+                    "z": 0.0
+                },
+                "rot": {
+                    "x": 0.0,
+                    "y": 0.0,
+                    "z": 0.0
+                }
+            }}
+        ]
+        for m in msg:
+            await ws.send(json.dumps(m))
+            print(json.loads(await ws.recv()))
+
+asyncio.get_event_loop().run_until_complete(hello())
+
+
